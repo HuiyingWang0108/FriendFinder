9ea46614c7c5e7a1f08caffa080bb5449f71d2bd
Huiying0108 <whymissu1228@gmail.com>
2019-01-14 13:48:36 -0800

    sec commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var port=process.env.PORT||8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/",function(req,res){
-    res.sendFile(path.join(__dirname,"./public/home.html"));
+    res.sendFile(path.join(__dirname,"./app/public/home.html"));
 });
 app.listen(port,function(){
     console.log("App listening on PORT: " + port);
